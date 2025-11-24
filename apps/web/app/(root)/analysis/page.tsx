2c3ea3ee6085f8715e511019f9f60098201b75f5
Karan Prajapat <karanpraja902@gmail.com>
2025-11-24 15:00:29 +0530

    feat: update GitHub App installation and issue reporting URLs from `ai-code-review-platform` to `ai-code-review`

--- apps/web/app/(root)/analysis/page.tsx
+++ apps/web/app/(root)/analysis/page.tsx
@@ -45,7 +45,7 @@ const Page = async (props: {
 
             <Link
               href={
-                "https://github.com/apps/ai-code-review-platform/installations/select_target"
+                "https://github.com/apps/ai-code-review/installations/select_target"
               }
               target="_blank"
             >
